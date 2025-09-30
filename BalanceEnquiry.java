aefb35a0fe00ee3b7aef69d572c609303d8bd1af
Prins Murchale <148212555+Princemurchale@users.noreply.github.com>
2025-09-30 21:16:13 +0530

    BalanceEnquiry.java

---
+++ BalanceEnquiry.java
@@ -0,0 +1,71 @@
+package bank.management.system;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class BalanceEnquiry extends JFrame implements ActionListener {
+    
+    JButton back;
+    String pinnumber;
+    
+    BalanceEnquiry(String pinnumber) {
+        this.pinnumber = pinnumber;
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 900, 900);
+        add(image); 
+        
+        back = new JButton("Back");
+        back.setBounds(355, 520, 150, 30);
+        back.addActionListener(this);
+        image.add(back);
+        
+        int balance = 0;
+        try {
+            Conn c = new Conn();
+            ResultSet rs = c.s.executeQuery("select * from bank where pin = '" + pinnumber + "'");
+            
+            while(rs.next()) {
+                if (rs.getString("type").equals("Deposit")) {
+                    balance += Integer.parseInt(rs.getString("amount"));
+                } else {
+                    balance -= Integer.parseInt(rs.getString("amount"));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Database error: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
+        JLabel text = new JLabel("Your Current Account balance is Rs " + balance);
+        text.setForeground(Color.WHITE);
+        text.setBounds(170, 300, 400, 30);
+        image.add(text);
+        
+        setSize(900, 900);
+        setLocation(300, 0);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setUndecorated(true);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        setVisible(false);
+        new Transactions(pinnumber).setVisible(true);
+    }
+    
+    public static void main(String args[]) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.out.println("Could not set system look and feel: " + e.getMessage());
+        }
+        
+        new BalanceEnquiry("");
+    }
+}
